dd2746516b5c298c00c27fa01b13e57c616045c2
agusRabu <agustinrabuffetti5@gmail.com>
2023-11-02 00:22:16 -0300

    Corrige admin.py

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
-# Register your models here.
+from productos.models import Producto
+admin.site.register(Producto)
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -3,4 +3,3 @@ from usuarios.models import Usuario
 from productos.models import Producto
 
 admin.site.register(Usuario)
-admin.site.register(Producto)
