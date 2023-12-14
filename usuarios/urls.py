7aad139b1d94f5d799c5294978818d3c5925eaa3
agusRabu <agustinrabuffetti5@gmail.com>
2023-12-13 21:05:48 -0300

    Agrega login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from usuarios.views import usuarios,altaUusarios
+from django.contrib.auth.views import LogoutView
+from usuarios.views import usuarios,altaUusarios, login
 
 urlpatterns = [
         path('', usuarios, name="usuarios"),
         path('altaUsuarios/', altaUusarios, name="altaUsuarios"),
+        path('login/', login, name="login"),
+        path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name="logout"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as django_login
 from django.db.models import Q
 from usuarios.models import Usuario
 
@@ -32,3 +34,20 @@ def altaUusarios(request):
         usuario = Usuario(nombre_usuario = nombre_usuario.lower(), nombre = nombre, apellido = apellido, password = password, ultima_vez = ultima_vez)
         usuario.save()
     return render(request, 'usuarios/altaUsuarios.html', {})
+
+def login(request):
+    
+    formulario = AuthenticationForm()
+    
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            usuario = formulario.cleaned_data.get('username')
+            contra  = formulario.cleaned_data.get('password')
+
+            user = authenticate(username = usuario, password = contra)
+            django_login(request, user)
+
+            return redirect('inicio')
+    
+    return render(request, 'usuarios/login.html', {'formulario_de_login': formulario})
